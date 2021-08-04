3723c94c8d768519ba3dced64d50cee826ad7463
DAXILSAXENA <daxildaxil@gmail.com>
2021-08-04 23:44:28 +0530

    [Daxil] Added:Welcome message in master branch

---
+++ src/main/java/com/bridgelabz/addressbooksystem/addressbookcontroller/AddressBookSystem.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbooksystem.addressbookcontroller;
+
+public class AddressBookSystem {
+	public static void main(String args[]) {
+		System.out.println("Welcome to Address Book Program:");
+	}
+}
